d1392f9111d60b16309c1ba6b497be924e81776a
starfigd <starfigd@student.21-school.ru>
2024-11-29 15:22:12 +0700

    edit deployment target version

---
+++ ToDoList/Model/AlamofireService.swift
@@ -0,0 +1,8 @@
+//
+//  AlamofireService.swift
+//  ToDoList
+//
+//  Created by Иван Бурцев on 29.11.2024.
+//
+
+import Foundation
---
+++ ToDoList/Model/Entity.swift
@@ -0,0 +1,8 @@
+//
+//  Entity.swift
+//  ToDoList
+//
+//  Created by Иван Бурцев on 29.11.2024.
+//
+
+import Foundation
